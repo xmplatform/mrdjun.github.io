122033681b82b823ddadbf3ff35502c700febdd6
mrdjun <mr.djun@qq.com>
2019-09-11 21:36:03 +0800

    ??INIT

--- blog-push.bat
+++ blog-push.bat
@@ -2,7 +2,7 @@
 
 git remote add origin "https://github.com/mrdjun/mrdjun.github.io"
 git add .
-git commit -m "Init"
+git commit -m "??INIT"
 git pull origin master
 git push -u origin master -f
 pause
