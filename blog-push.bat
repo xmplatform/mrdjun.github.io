2f0ea8c7a95e21828b63afea8c87f3217c67ae4b
mrdjun <mr.djun@qq.com>
2019-12-06 10:31:15 +0800

    INIT

--- blog-push.bat
+++ blog-push.bat
@@ -2,7 +2,7 @@
 
 git remote add origin "https://github.com/mrdjun/fun-generator"
 git add .
-git commit -m "??INIT"
+git commit -m "INIT"
 git pull origin master
 git push -u origin master -f
 pause
